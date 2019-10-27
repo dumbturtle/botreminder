136e2fdd591a30c6989ca194f936c38f0f1afcdf
Dumbturtle <semenlinux@gmail.com>
2019-10-27 19:55:25 +0300

    Распределено по папкам, приведено к pep8

---
+++ bothandlers/keyboards.py
@@ -0,0 +1,48 @@
+from telegram import ReplyKeyboardMarkup, KeyboardButton
+from datetime import datetime, timedelta
+
+from settings import settings
+
+
+def starting_keyboard():
+    starting_keyboard = ReplyKeyboardMarkup(
+        [['Хочу пользоваться!', 'Расхотел']], resize_keyboard=True)
+    return starting_keyboard
+
+
+def reminder_keyboard():
+    work_keyboard = ReplyKeyboardMarkup([['Добавить напоминание', 'Список напоминаний',
+                                          'Удалить напоминание']], resize_keyboard=True)
+    return work_keyboard
+
+
+def reminder_add_day_keyboard():
+    today_date = datetime.today()
+    today_date_keyboard = today_date.strftime("%d-%m-%Y")
+    tomorrow_date_keyboard = (
+        today_date + timedelta(days=1)).strftime("%d-%m-%Y")
+    reminder_add_day_keyboard = ReplyKeyboardMarkup([[today_date_keyboard, tomorrow_date_keyboard,
+                                                      'Ввести дату']], resize_keyboard=True)
+    return reminder_add_day_keyboard
+
+
+def reminder_add_digital_period_keyboard(start, end, keyboard_long, keyboard_step):
+    end += 1
+    key = []
+    for keys in range(start, end, keyboard_long):
+        key.append([f'{key}' for key in range(
+            keys, keys + keyboard_long, keyboard_step) if key < end])
+
+    digital_period_keyboard = ReplyKeyboardMarkup(key, resize_keyboard=True)
+    return digital_period_keyboard
+
+
+def remind_list_for_delete_keyboard(remind_list):
+    keyboard = ReplyKeyboardMarkup([remind_list], resize_keyboard=True)
+    return keyboard
+
+
+def remind_confirm_for_delete_keyboard():
+    delete_keyboard = ReplyKeyboardMarkup(
+        [['Да', 'Нет']], resize_keyboard=True)
+    return delete_keyboard
--- database/db.py
+++ database/db.py
@@ -1,52 +1,50 @@
 
-from sqlalchemy import Column, Integer, String, ForeignKey, DateTime
+from sqlalchemy import Column, Integer, String, ForeignKey, DateTime, create_engine
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
+from settings import connect_settings
 
 Base = declarative_base()
-database_engine = create_engine("sqlite:///bot_db.db")
+database_engine = create_engine(connect_settings.DATABASE)
 database_Session = sessionmaker(bind=database_engine)
 database_session = database_Session()
 
+
 class User(Base):
-	__tablename__ = 'users'
-	id = Column(Integer, primary_key=True)
-	telegramm_user_id = Column(Integer)
-	first_name = Column(String)
-	last_name = Column(String)
-	username = Column(String)
-	chat_id = Column(Integer)
-
-	def __init__(self, telegramm_user_id, first_name, last_name, username, chat_id):
-		self.telegramm_user_id = telegramm_user_id
-		self.first_name = first_name
-		self.last_name = last_name
-		self.username = username
-		self.chat_id = chat_id
-	
-	def __repr__(self):
-		return "<User('%d','%d','%s', '%s', '%s', '%d')>" % (self.id, self.telegramm_user_id, self.first_name, self.last_name, self.username, self.chat_id)
-
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True)
+    telegramm_user_id = Column(Integer)
+    first_name = Column(String)
+    last_name = Column(String)
+    username = Column(String)
+    chat_id = Column(Integer)
+    def __init__(self, telegramm_user_id, first_name, last_name, username, chat_id):
+        self.telegramm_user_id = telegramm_user_id
+        self.first_name = first_name
+        self.last_name = last_name
+        self.username = username
+        self.chat_id = chat_id
+    def __repr__(self):
+        return "<User('%d','%d','%s', '%s', '%s', '%d')>".format(self.id, self.telegramm_user_id, self.first_name, self.last_name, self.username, self.chat_id)
 
 
 class Reminder_data(Base):
-	__tablename__ = 'remainders'
-	id = Column(Integer, primary_key=True)
-	user_id = Column(Integer,index=True)
-	comment = Column(String)
-	date_remind = Column(DateTime)
-	status = Column(String)
+    __tablename__ = 'remainders'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer,index=True)
+    comment = Column(String)
+    date_remind = Column(DateTime)
+    status = Column(String)
 
-	def __init__(self, user_id, comment, date_remind, status):
-		self.user_id = user_id
-		self.comment = comment
-		self.date_remind = date_remind
-		self.status = status
+    def __init__(self, user_id, comment, date_remind, status):
+        self.user_id = user_id
+        self.comment = comment
+        self.date_remind = date_remind
+        self.status = status
 
-	def __repr__(self):
-		return "<Data('%d', '%d', '%s', '%s', '%s')>" % (self.id, self.user_id, self.comment, self.date_remind, self.status)
+    def __repr__(self):
+        return "<Data('%d', '%d', '%s', '%s', '%s')>".format(self.id, self.user_id, self.comment, self.date_remind, self.status)
 
 
 
