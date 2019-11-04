c1817e8ab72556c67c40f52857b0c446264348b1
Aleksandr Semenov <semenlinux@gmail.com>
2019-11-04 16:39:58 +0000

    Пробую привести к PEP

--- reminds_handlers.py
+++ reminds_handlers.py
@@ -1,46 +1,42 @@
 
 import logging
 import schedule
-from datetime import datetime
+from datetime import datetime, timedelta
 from sqlalchemy.exc import SQLAlchemyError
 from telegram import Bot, utils
 
 from settings import connect_settings, settings
 from database.modeldb import database_session, User, Reminder_data
 
+logging.basicConfig(format='%(name)s - %(levelname)s - %(message)s', level=logging.INFO, filename='bot_r.log')
 
 def check_time_reminder():
     reminders_list = database_session.query(Reminder_data).filter(
         Reminder_data.status == 'active').all()
     for reminder in reminders_list:
         if reminder.date_remind.strftime("%d-%m-%Y %H:%M") ==\
-           datetime.now().strftime("%d-%m-%Y %H:%M"):
+         ((datetime.now()+timedelta(hours=3)).strftime("%d-%m-%Y %H:%M")):
             sending_notification_reminder(
                 reminder.user_id,
                 reminder.date_remind,
                 reminder.comment)
             change_reminder_status(reminder.id)
 
-
 def sending_notification_reminder(user_id, reminder_date, comment):
     user_information = database_session.query(
         User).filter(User.id == user_id).first()
     bot_proxy = utils.request.Request(
         proxy_url=connect_settings.PROXY,
-        urllib3_proxy_kwargs=connect_settings.PROXY_ACCOUNT
-        )
+        urllib3_proxy_kwargs=connect_settings.PROXY_ACCOUNT)
     reminder_bot = Bot(
         token=connect_settings.API_KEY,
-        request=bot_proxy
-        )
+        request=bot_proxy)
     message_text = settings.REMIND_MESSAGE_TEXT.format(
         reminder_date,
-        comment
-        )
+        comment)
     reminder_bot.sendMessage(
         chat_id=user_information.chat_id,
-        text=message_text
-        )
+        text=message_text)
 
 
 def change_reminder_status(remind_id):
